b0bd5e2c25aec5d820cfd8c33195d51575539259
Marion Terray <mterray@gmail.com>
2023-05-07 18:56:56 +0200

    tech page

---
+++ tryingthings/src/composants/TechData.js
@@ -0,0 +1,26 @@
+import LaunchImage from '../Images/ImageTechnology/image-launch-vehicle-portrait.jpg';
+import SpacePortImage from '../Images/ImageTechnology/image-spaceport-portrait.jpg';
+import CapsuleImage from '../Images/ImageTechnology/image-space-capsule-portrait.jpg';
+
+export const TechData = [
+    {
+        id: 1,
+        title: "LAUNCH VEHICLE",
+        desc: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 meters tall, it's quite an awe-inspiring sight on the launch pad !",
+        image: LaunchImage,
+    },
+
+    {
+        id: 2,
+        title: "SPACEPORT",
+        desc: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth's rotation for launch. ",
+        image: SpacePortImage,
+    },
+
+    {
+        id: 3,
+        title: "Space capsule",
+        desc: " A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained. ",
+        image: CapsuleImage,
+    }
+]
--- tryingthings/src/composants/Technology.js
+++ tryingthings/src/composants/Technology.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/Technology.css';
+import { TechData } from './TechData.js';
 
 function Technology() {
+
+  let  [ ElClicked, setElClicked ] = useState(TechData[0])
+
+  function handleClick(e) { 
+      setElClicked(TechData[e.target.id])
+    }
+
   return (
-    <div className='technology'>Technology</div>
+    <div className='technology'>
+      <h2>03 Space launch 101</h2>
+      <div className='technology-detail'>
+        <div className='technology-btn'>
+          <button id='0' onClick={handleClick}>1</button>
+          <button id='1' onClick={handleClick}>2</button>
+          <button id='2' onClick={handleClick}>3</button>
+        </div>
+        <div className='technology-description'>
+          <p>THE TERMINOLOGY ...</p>
+          <h3>{ElClicked.title}</h3>
+          <p>{ElClicked.desc}</p>
+        </div>
+        <div className='technology-image'>
+        <img src={ElClicked.image} alt={ElClicked.name}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
