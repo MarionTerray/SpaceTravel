0c98cdb1cfd35577d740360d3e02d6c71132f8c2
Marion Terray <mterray@gmail.com>
2023-05-30 11:16:28 +0200

    crew page

--- tryingthings/src/composants/Crew.js
+++ tryingthings/src/composants/Crew.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/Crew.css';
+import { CrewData } from './CrewData';
 
 function Crew() {
+  let  [ ElClicked, setElClicked ] = useState(CrewData[0])
+
+  function handleClick(e) { 
+      setElClicked(CrewData[e.target.id])
+    }
+
   return (
-    <div className='crew'>Crew</div>
+    <div className='crew'>
+      <h2>02 MEET YOUR CREW</h2>
+        <div className='crewContent'>
+          <div className='crewPresentation'>
+            <p>{ElClicked.poste}</p>
+            <h3>{ElClicked.name}</h3>
+            <p>{ElClicked.descr}</p>
+          </div>
+          <img src={ElClicked.image} alt={ElClicked.name} />
+          <div className='crewBtn'>
+            <button id='0' onClick={handleClick}></button>
+            <button id='1' onClick={handleClick}></button>
+            <button id='2' onClick={handleClick}></button>
+            <button id='3' onClick={handleClick}></button>
+          </div>
+          
+        </div>
+    
+    
+    </div>
   )
 }
 
---
+++ tryingthings/src/composants/CrewData.js
@@ -0,0 +1,35 @@
+import AnsariImage from '../Images/ImageCrew/image-anousheh-ansari.png';
+import HurleyImage from '../Images/ImageCrew/image-douglas-hurley.png';
+import ShuttleworthImage from '../Images/ImageCrew/image-mark-shuttleworth.png';
+import GloverImage from '../Images/ImageCrew/image-victor-glover.png';
+
+export const CrewData = [
+    {
+        id : 1,
+        poste: "Flight engineer",
+        name: "ANOUSHEH ANSARI",
+        image: AnsariImage,
+        descr: "Anousheh Ansari is an Iranian American engineer and cofounder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-founded woman to fly to the ISS, and the first Iranian in space.",
+    },
+    {
+        id : 2,
+        poste: "Commander",
+        name: "DOUGLAS HURLEY",
+        image: HurleyImage,
+        descr: "Commander Douglas Hurley Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
+    },
+    {
+        id : 3,
+        poste: "Mission Specialist",
+        name: "MARK SHUTTLEWORTH",
+        image: ShuttleworthImage,
+        descr: "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
+    },
+    {
+        id : 4,
+        poste: "Pilot",
+        name: "VICTOR GLOVER",
+        image: GloverImage,
+        descr: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
+    },
+]
